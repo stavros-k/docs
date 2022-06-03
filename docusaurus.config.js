9b4d2fadfd9c655197df4c52d4a9f00478d85951
Stavros kois <s.kois@outlook.com>
2022-06-03 22:57:00 +0300

    add /tree/master

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/stavros-k/docs',
+            'https://github.com/stavros-k/docs/tree/master/',
         },
         blog: false,
         theme: {
