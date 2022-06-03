196f80015517c74c2c0c240ce87870620c56b74c
Stavros kois <s.kois@outlook.com>
2022-06-03 22:56:19 +0300

    remvoe second docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/stavros-k/docs/docs',
+            'https://github.com/stavros-k/docs',
         },
         blog: false,
         theme: {
